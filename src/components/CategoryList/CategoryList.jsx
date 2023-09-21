846f4de7bc2164a8a186d38c3136311325871628
ayeshaalpona <ayeshasiddika9191@gmail.com>
2023-09-21 21:26:06 +0600

    navbar customaization

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+
+
+const CategoryList = () => {
+    return (
+        <div className="">
+            <h2 className="text-5xl text-center mb-5">Category List</h2>
+            <p className="text-center ">Explore thousands of job opportunities with all the information you need. It's your future.</p>
+        </div>
+    );
+};
+
+export default CategoryList;
